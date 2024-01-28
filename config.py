d47b5057aea1a9ba12c158b4a1ec77fa9b9e24f3
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-01-28 19:19:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5061749246").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5061749246 ").split()))
 for x in ALTRON:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="5061749246 1983816571"))
+OWNER_ID = int(getenv("OWNER_ID", default="5061749246"))
 SUDO_USERS.append(OWNER_ID)
 
 
